289d209e437a330ec6a36c6f11c1a27157ed0657
Romain SOMMERARD <rsommerard@gmail.com>
2016-05-02 22:19:31 +0200

    Separate weave and docker-machine

---
+++ create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#docker-machine create --driver virtualbox master
+docker-machine create --driver virtualbox node1
+docker-machine create --driver virtualbox node2
+
--- install.sh
+++ install.sh
@@ -9,7 +9,3 @@ echo "Weave installation..."
 curl -L git.io/weave -o /usr/local/bin/weave
 chmod +x /usr/local/bin/weave
 
-docker-machine create --driver virtualbox master
-docker-machine create --driver virtualbox node2
-docker-machine create --driver virtualbox node1
-
